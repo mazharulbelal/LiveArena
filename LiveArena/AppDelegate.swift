03525757b8a66ab40c26ecf213fb9f98e8f902bd
Mazharul Islam <mazharul.belal@gmail.com>
2019-08-10 15:01:30 +0600

    // Firebase Install

--- LiveArena/AppDelegate.swift
+++ LiveArena/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+
 
             
             // ViewController Add Programmly
