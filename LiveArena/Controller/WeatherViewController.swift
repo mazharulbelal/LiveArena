6bc89a9158d7d47c2fa5a9b018f90f9b90f01ad1
Mazharul Belal <soft.belal@gmail.com>
2019-09-17 20:18:10 +0600

    CollectionViewController

--- LiveArena/Controller/WeatherViewController.swift
+++ LiveArena/Controller/WeatherViewController.swift
@@ -5,63 +5,76 @@
 //  Created by Mazharul Belal on 9/12/19.
 //
 
-import Foundation
 import UIKit
 
-class WeatherViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class WeatherViewController: UIViewController {
     
-    let CastData = WeatherData.getWeatherData()
-    let contactsTableView = UITableView()
-    
-    
-    
+        let newCollection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collection = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: layout)
+        layout.scrollDirection = .horizontal
+        collection.backgroundColor = UIColor.gray
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.isScrollEnabled = true
+        return collection
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         navigationItem.title = "Weather"
-        view.backgroundColor = .white
-        view.addSubview(contactsTableView)
         
-        contactsTableView.translatesAutoresizingMaskIntoConstraints = false
-        contactsTableView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
-        contactsTableView.leftAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leftAnchor).isActive = true
-        contactsTableView.rightAnchor.constraint(equalTo:view.safeAreaLayoutGuide.rightAnchor).isActive = true
-        contactsTableView.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        newCollection.delegate = self
+        newCollection.dataSource = self
         
         
+        newCollection.register(WeatherCell.self, forCellWithReuseIdentifier: "WeatherCell")
         
-        contactsTableView.dataSource = self
-        contactsTableView.delegate = self
-        contactsTableView.register(WeatherCell.self, forCellReuseIdentifier: "WeatherCell")
+        view.addSubview(newCollection)
+        AutoLayout()
         
-        
-        
-        
-       
     }
     
+}
+
+extension WeatherViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return CastData.count
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
-        
-        
-        cell.contact = CastData[indexPath.row]
-        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = newCollection.dequeueReusableCell(withReuseIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        cell.backgroundColor  = .white
+        cell.layer.cornerRadius = 5
+        cell.layer.shadowOpacity = 3
+        cell.imageView.image = UIImage(named: "user")
         return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 150)
     }
     
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3)
+    }
+    
+    
+    
+    
+    func AutoLayout(){
+        
+        
+        newCollection.translatesAutoresizingMaskIntoConstraints = false
+        newCollection.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
+        newCollection.leftAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        newCollection.rightAnchor.constraint(equalTo:view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        newCollection.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        
+    }
     
     
 }
