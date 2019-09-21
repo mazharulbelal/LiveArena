ea89366fa3f0d8731cc0a0b5459be564c493f96c
Mazharul Belal <soft.belal@gmail.com>
2019-09-21 23:42:50 +0600

    WeatherSection

--- LiveArena/Cell/WeatherViewCell.swift
+++ LiveArena/Cell/WeatherViewCell.swift
@@ -1,16 +1,18 @@
 //
-//  WeatherCell.swift
+//  WeatherViewCell.swift
 //  LiveArena
 //
-//  Created by Mazharul Belal on 20/9/19.
+//  Created by Mazharul Belal on 21/9/19.
 //  Copyright © 2019 Mazharul Belal. All rights reserved.
 //
 
 import UIKit
 
-class WeatherCell: UICollectionViewCell {
+class WeatherViewCell: UICollectionViewCell {
     
-     static let name = "WeatherCell"
+    
+    
+    static var name = "WeatherViewCell"
     
     @IBOutlet weak var backView : UIView!
     @IBOutlet weak var dateLabel : UILabel!
@@ -20,13 +22,11 @@ class WeatherCell: UICollectionViewCell {
     @IBOutlet weak var maxWeather : UILabel!
     @IBOutlet weak var windSpeed : UILabel!
     @IBOutlet weak var weatherImage : UILabel!
-    
-    
-   
-    
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        
         
     }
 
--- LiveArena/Controller/WeatherViewController.swift
+++ LiveArena/Controller/WeatherViewController.swift
@@ -9,64 +9,123 @@ import UIKit
 
 class WeatherViewController: UIViewController {
     
-    let bounds = UIScreen.main.bounds
     
-    var WeatherData : [WeatherCategory] = []
+    var NameArray = ["AAA","BBB"]
+
+    var weatherData : [WeatherCategory] = []
+   
     
-    let newCollection: UICollectionView = {
-    let layout = UICollectionViewFlowLayout()
-    let collection = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: layout)
-        layout.scrollDirection = .vertical
-        collection.backgroundColor = UIColor.gray
-        collection.translatesAutoresizingMaskIntoConstraints = false
-        collection.isScrollEnabled = true
-        return collection
-    }()
-    
+        let mCollectionView: UICollectionView = {
+            let layout = UICollectionViewFlowLayout()
+            let collection = UICollectionView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), collectionViewLayout: layout)
+            
+            layout.scrollDirection = .vertical
+            collection.backgroundColor = UIColor.white
+            collection.translatesAutoresizingMaskIntoConstraints = false
+            collection.isScrollEnabled = true
+            return collection
+        }()
+        
+        
+        
+        
+        
+        
+        
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        newCollection.delegate = self
-        newCollection.dataSource = self
+        self.weatherData = WeatherData().getWeatherData()
         
+        mCollectionView.delegate = self
+        mCollectionView.dataSource = self
         
-        newCollection.register(WeatherCell.self, forCellWithReuseIdentifier: WeatherCell.name)
+        view.addSubview(mCollectionView)
         
-        view.addSubview(newCollection)
+       
+        let cellNib = UINib(nibName: WeatherViewCell.name, bundle: nil)
+        self.mCollectionView.register(cellNib, forCellWithReuseIdentifier: WeatherViewCell.name)
         
         
     }
+    
+    
+    func getCellSize () -> CGSize {
+        let vw = CGFloat(self.view.frame.width)
+        let margins: CGFloat = 10.0 + 10.0
+        let gapBetweenItems: CGFloat = 0.0
+        let width: CGFloat = vw - margins - gapBetweenItems
+        let numberOfItemsPerRow: CGFloat = 1
+        return CGSize(width: (width/numberOfItemsPerRow) - 5.0, height: 210.00)
+    }
+    
+    
+    
     
 }
 
-extension WeatherViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+  
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+    
+extension WeatherViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = newCollection.dequeueReusableCell(withReuseIdentifier: WeatherCell.name, for: indexPath) as! WeatherCell
+    
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        cell.dateLabel.text =
+        return weatherData.count
+    }
+    
+    
+    
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return self.getCellSize()
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets (top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+    }
+    
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherViewCell.name, for: indexPath) as! WeatherViewCell
+        
+         let weatherdata =  self.weatherData[indexPath.row]
+        
+            cell.dateLabel.text = weatherdata.dateData
+            
         
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: bounds.width, height: 200)
-    }
-    
- 
-    
-  
     
     
     
     
-   
+    
     
     
 }
+
